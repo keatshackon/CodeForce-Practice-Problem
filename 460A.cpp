1e10e690ceb9e0e309d5ffaf11c87d9487a25c2f
keatshackon <keatshackon@gmail.com>
2021-02-26 15:29:08 +0530

    460A.cpp added

---
+++ 460A.cpp
@@ -0,0 +1,36 @@
+/*                  #####################################
+                    #           Nickname:keats          #
+                    #           User:keats92            #
+                    #####################################            
+*/
+
+#include <bits/stdc++.h> 
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define pb push_back
+#define N 10001
+#define lb lower_bound
+#define M 100000000
+#define ub upper_bound
+#define mp make_pair
+#define LD long double
+#define f first
+#define se second 
+
+int main() {
+    fast
+    #ifdef _DEBUG
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    
+    ll q,x,y,z,i,j,k,l,m,t,r,d,n,a,b,c;
+
+	cin>>t>>n;
+
+	cout<< t + (t-1)/(n-1);
+
+	
+    return 0;
+}
